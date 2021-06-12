167f4dc73962b8db775ff8d660e4702017e7ac00
surender.lamba <surender.lamba@rakuten.com>
2021-06-12 23:25:43 +0900

    First add

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3
-poetry install
+$HOME/.poetry/bin/poetry install
 hypercorn main:app --bind "[::]:8000"
