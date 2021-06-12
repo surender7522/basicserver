e065c9edcf21294f6c59f5515303292fd92ecacb
surender.lamba <surender.lamba@rakuten.com>
2021-06-13 00:53:31 +0900

    First add

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from fastapi import FastAPI
-import requests
 import json
+import os
+
+import requests
+from fastapi import BackgroundTasks, FastAPI
+
 app = FastAPI()
+url = os.getenv("url", "")
 
 
 def authorize():
@@ -19,18 +23,46 @@ def authorize():
             }
         }
     }
-    r=requests.post("http:///identity/v3/auth/tokens", json=json.dumps(js))
+    r = requests.post(
+        "http://{0}/identity/v3/auth/tokens".format(url), data=json.dumps(js)
+    )
     print(r.status_code)
     print(r.headers)
     print(type(r.headers))
+    return r.headers.get("X-Subject-Token", "")
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def write_notification(uid: str):
+    token = authorize()
+    x = {
+        "events": [
+            {
+                "name": "network-changed",
+                "server_uuid": uid,
+                "tag": uid,
+                "status": "completed",
+            }
+        ]
+    }
+    headers = {"X-Auth-Token": token}
+    r = requests.post(
+        "http://{0}/compute/v2.1/os-server-external-events".format(url),
+        data=json.dumps(x),
+        headers=headers,
+    )
+    # r = requests.get("http://{0}/compute/v2.1/servers".format(url), headers=headers)
+    print(r.status_code)
+    print(r.json())
+
+
+@app.get("/{uid}")
+async def root(uid: str, background_tasks: BackgroundTasks):
+    background_tasks.add_task(write_notification, uid=uid)
+    return {"message": "token", "uid": uid}
 
 
 @app.post("/{uid}")
-async def root(uid: str):
-    return {"message": uid}
-authorize()
+async def root(uid: str, background_tasks: BackgroundTasks):
+    background_tasks.add_task(write_notification, uid=uid)
+
+    return {"message": "token", "uid": uid}
