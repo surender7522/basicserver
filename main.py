1733e78b086791c7727c5a09d73d29727636e9a7
surender.lamba <surender.lamba@rakuten.com>
2021-06-13 00:36:53 +0900

    First add

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
 from fastapi import FastAPI
-
+import requests
+import json
 app = FastAPI()
+
+
+def authorize():
+    js = {
+        "auth": {
+            "identity": {
+                "methods": ["password"],
+                "password": {
+                    "user": {
+                        "name": "admin",
+                        "domain": {"id": "default"},
+                        "password": "secret",
+                    }
+                },
+            }
+        }
+    }
+    r=requests.post("http:///identity/v3/auth/tokens", json=json.dumps(js))
+    print(r.status_code)
+    print(r.headers)
+    print(type(r.headers))
 
 
 @app.get("/")
@@ -11,3 +33,4 @@ async def root():
 @app.post("/{uid}")
 async def root(uid: str):
     return {"message": uid}
+authorize()
